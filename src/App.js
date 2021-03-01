b776070529a808f4a604b917c2a88c81e9d0f999
brcolt1 <brcolturato@gmail.com>
2021-02-28 22:46:31 -0300

    Limpando arquivo padrão

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+/**
+ * Function component do react.
+ * As props devem ser imutáveis!!
+ */
+function Welcome(props) {
+    return(
+      <p>Hello {props.children}!</p>
+    );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return(
+        <div>
+            <Welcome>Bruno</Welcome>
+        </div>
+    );
 }
 
 export default App;
